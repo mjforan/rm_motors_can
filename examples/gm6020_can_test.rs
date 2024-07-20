5b86a40e2d5106fede0a1c72ed9cd6e0d8c635e5
Matthew Foran <matthewjforan@gmail.com>
2024-07-19 20:29:45 -0400

    Basic commands working. Renamed to lib.rs to match Rust convention

--- examples/gm6020_can_test.rs
+++ examples/gm6020_can_test.rs
@@ -1,19 +1,23 @@
-use std::thread;
 use gm6020_can::{CmdMode, Gm6020Can};
+use std::ffi::CString;
+use std::thread;
 
+
+
+const RATE: u64 = 100;
+const PERIOD: u64 = (1.0f32/(RATE as f32))as u64;
 fn main() {
+    let ifname: std::ffi::CString = CString::new("can0").expect("CString::new failed");
+    let gmc: *mut Gm6020Can = gm6020_can::init(ifname.as_ptr());
 
-    let delay = std::time::Duration::from_secs(1);
+    gm6020_can::cmd_single(gmc, CmdMode::Voltage, 1_u8, 5_f64);
+    gm6020_can::run(gmc, PERIOD);
+    thread::sleep(std::time::Duration::from_secs(2));
+    gm6020_can::cmd_single(gmc, CmdMode::Voltage, 1_u8, 10_f64);
+    thread::sleep(std::time::Duration::from_secs(2));
 
-    let gmc: *mut Gm6020Can = gm6020_can::init("can0");
-    thread::spawn(gm6020_can::run(gmc));
-    gm6020_can::cmd_single(gmc, CmdMode::Voltage, 0_u8, 5_f64);
-    thread::sleep(delay);
-    gm6020_can::cmd_single(gmc, CmdMode::Voltage, 0_u8, 0_f64);
-    thread::sleep(delay);
-    gm6020_can::cmd_single(gmc, CmdMode::Voltage, 0_u8, -5_f64);
-    thread::sleep(delay);
-    gm6020_can::cmd_single(gmc, CmdMode::Voltage, 0_u8, 0_f64);
+    gm6020_can::cmd_single(gmc, CmdMode::Voltage, 1_u8, 5_f64);
+    thread::sleep(std::time::Duration::from_secs(2));
 
-    Ok(())
+    gm6020_can::cmd_single(gmc, CmdMode::Voltage, 1_u8, 0_f64);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::time::Duration;
 use embedded_can::{Frame as EmbeddedFrame, StandardId};
 use socketcan::{CanDataFrame, CanFilter, CanFrame, CanSocket, Frame, Socket, SocketOptions};
 use core::slice;
@@ -5,6 +6,8 @@ use std::ptr::null;
 use std::time::SystemTime;
 use std::ffi::CStr;
 use std::os::raw::c_char;
+use std::thread;
+
 
 const FB_ID_BASE: u16 = 0x204;
 const CMD_ID_V_L: u16 = 0x1ff;
@@ -73,13 +76,15 @@ fn _init(interface: &str) -> Result<Box<Gm6020Can>, String> {
     let mut gm6020_can: Box<Gm6020Can> = Box::new(Gm6020Can::default()); // TODO technically a memory leak - also make sure socket is closed
     gm6020_can.as_mut().socket = Some(CanSocket::open(&interface).map_err(|err| err.to_string())?);
     let filter = CanFilter::new(FB_ID_BASE as u32, 0xffffffff - 0xf);
-    gm6020_can.as_ref().socket.as_ref().unwrap().set_filters(&[filter]).map_err(|err| err.to_string())?; // TODO why does this not need to be mutable? Is it acutally working on the socket or returning a copy?
+    //let filter = CanFilter::new(517 as u32, 0xffffffff);
+    gm6020_can.as_ref().socket.as_ref().unwrap().set_filters(&[filter]).map_err(|err| err.to_string())?;
     return Ok(gm6020_can);
+    
 }
 
 // TODO break into separate thread for receiving
 #[no_mangle]
-pub extern "C" fn run(gm6020_can: *mut Gm6020Can) -> i8{
+pub extern "C" fn run(gm6020_can: *mut Gm6020Can, period: u64) -> i8{
     let handle: &mut Gm6020Can;
     if gm6020_can.is_null(){
         println!("Invalid handle (null pointer)");
@@ -88,11 +93,14 @@ pub extern "C" fn run(gm6020_can: *mut Gm6020Can) -> i8{
     else{
         handle = unsafe{&mut *gm6020_can};
     }
-    _run(handle).map_or_else(|e| {eprintln!("{}", e); -1_i8}, |_| 0_i8)
+    let handle = thread::spawn( move || _run(handle, period).map_or_else(|e| {eprintln!("{}", e); -1_i8}, |_| 0_i8));
+    8
+    
 }
-fn _run(gm6020_can: &mut Gm6020Can) -> Result<(), String>{
+fn _run(gm6020_can: &mut Gm6020Can, period: u64) -> Result<(), String>{
     loop {
         _run_once(gm6020_can)?;
+        thread::sleep(std::time::Duration::from_secs(period));
     }
 }
 #[no_mangle]
@@ -108,12 +116,41 @@ pub extern "C" fn run_once(gm6020_can: *mut Gm6020Can) -> i8{
     _run_once(handle).map_or_else(|e| {eprintln!("{}", e); -1_i8}, |_| 0_i8)
 }
 fn _run_once(gm6020_can: &mut Gm6020Can) -> Result<(), String>{
-    match gm6020_can.socket.as_ref().unwrap().read_frame() {
+    match gm6020_can.socket.as_ref().unwrap().read_frame_timeout(Duration::from_millis(10)) {
         Ok(CanFrame::Data(frame)) => rx_fb(gm6020_can, frame),
-        Ok(CanFrame::Remote(frame)) => println!("{:?}", frame),
-        Ok(CanFrame::Error(frame)) => println!("{:?}", frame),
+        Ok(CanFrame::Remote(frame)) => println!("CanRemoteFrame: {:?}", frame),
+        Ok(CanFrame::Error(frame)) => println!("CanErrorFrame: {:?}", frame),
         Err(err) => eprintln!("{}", err),
     };
+    match gm6020_can.socket.as_ref().unwrap().read_frame_timeout(Duration::from_millis(10)) {
+        Ok(CanFrame::Data(frame)) => rx_fb(gm6020_can, frame),
+        Ok(CanFrame::Remote(frame)) => println!("CanRemoteFrame: {:?}", frame),
+        Ok(CanFrame::Error(frame)) => println!("CanErrorFrame: {:?}", frame),
+        Err(err) => eprintln!("{}", err),
+    };
+    // TODO check which ones actually need to be sent
+/*
+    let mut send_low: bool = false;
+    let mut send_high: bool = false;
+    for cmd in cmds.into_iter(){
+        send_low |= cmd.0<=4;    
+        send_high |= cmd.0>4;
+        set_cmd(gm6020_can, cmd.0, mode, cmd.1)?;
+    }
+
+    match (send_low, send_high) {
+        (false, false) => Err("Not sending any command".to_string()),
+        (true,  false) => tx_cmd(gm6020_can, IdRange::Low, mode),
+        (false, true)  => tx_cmd(gm6020_can, IdRange::High, mode),
+        (true,  true)  => tx_cmd(gm6020_can, IdRange::Low, mode).and_then(|_| tx_cmd(gm6020_can, IdRange::Low, mode))
+    }
+
+    */
+    for mode in [CmdMode::Voltage, CmdMode::Current]{
+        for range in [IdRange::Low, IdRange::High]{
+            tx_cmd(gm6020_can, range, mode)?;
+        }
+    }
     Ok(())
 }
 
@@ -148,7 +185,6 @@ pub extern "C" fn cmd_single(gm6020_can: *mut Gm6020Can, mode: CmdMode, id: u8, 
 fn _cmd_single(gm6020_can: &mut Gm6020Can, mode: CmdMode, id: u8, cmd: f64) -> Result<(), String> {
     if id<ID_MIN || id>ID_MAX { return Err(format!("id out of range [{}, {}]: {}", ID_MIN, ID_MAX, id)); }
     set_cmd(gm6020_can, id, mode, cmd)?;
-    tx_cmd(gm6020_can, match id > 4 {false=>IdRange::Low, true=>IdRange::High}, mode)?;
     Ok(())
 }
 
@@ -171,28 +207,20 @@ pub extern "C" fn cmd_multiple(gm6020_can: *mut Gm6020Can, mode: CmdMode, cmds: 
     _cmd_multiple(handle, mode, cmds3).map_or_else(|e| {eprintln!("{}", e); -1_i8}, |_| 0_i8)
 }
 fn _cmd_multiple(gm6020_can: &mut Gm6020Can, mode: CmdMode, cmds: Vec<(u8, f64)> ) -> Result<(), String> {
-    let mut send_low: bool = false;
-    let mut send_high: bool = false;
     for cmd in cmds.into_iter(){
-        send_low |= cmd.0<=4;    
-        send_high |= cmd.0>4;
         set_cmd(gm6020_can, cmd.0, mode, cmd.1)?;
     }
-
-    match (send_low, send_high) {
-        (false, false) => Err("Not sending any command".to_string()),
-        (true,  false) => tx_cmd(gm6020_can, IdRange::Low, mode),
-        (false, true)  => tx_cmd(gm6020_can, IdRange::High, mode),
-        (true,  true)  => tx_cmd(gm6020_can, IdRange::Low, mode).and_then(|_| tx_cmd(gm6020_can, IdRange::Low, mode))
-    }
+    Ok(())
 }
 
 fn tx_cmd(gm6020_can: &mut Gm6020Can, id_range: IdRange, mode: CmdMode) -> Result<(), String> {
+    // ToDo commented for testing
+    /*
     for (i, fb) in (&gm6020_can.feedbacks[((id_range as u8) * 4) as usize .. (4 + (id_range as u8)*4) as usize]).iter().enumerate() {
-        if gm6020_can.commands[i] != 0 && fb.0.ok_or_else(|| Err::<(), String>(format!("Motor {} never responded. Did you enter the `run` loop?", i+ID_MIN))).unwrap().elapsed().map_err(|err| err.to_string())?.as_millis() >= 10 {
-            return Err(format!("Motor {} not responding. Did you enter the `run` loop?", i+ID_MIN));
+        if gm6020_can.commands[i] != 0 && fb.0.ok_or_else(|| Err::<(), String>(format!("Motor {} never responded. Did you enter the `run` loop?", (i as u8)+ID_MIN))).unwrap().elapsed().map_err(|err| err.to_string())?.as_millis() >= 10 {
+            return Err(format!("Motor {} not responding. Did you enter the `run` loop?", (i as u8)+ID_MIN));
         }
-    }
+    }*/
 
     let id: u16 = match (id_range, mode) {
         (IdRange::Low,  CmdMode::Voltage) => CMD_ID_V_L,
@@ -207,6 +235,7 @@ fn tx_cmd(gm6020_can: &mut Gm6020Can, id_range: IdRange, mode: CmdMode) -> Resul
         StandardId::new(id).unwrap(),
         &[(cmds[0]>>8) as u8, cmds[0] as u8, (cmds[1]>>8) as u8, cmds[1] as u8, (cmds[2]>>8) as u8, cmds[2] as u8, (cmds[3]>>8) as u8, cmds[3] as u8])
         .ok_or_else(|| Err::<CanFrame, String>("Failed to open socket".to_string())).unwrap();
+    println!("transmitting frame: {:?}", frame);
     gm6020_can.socket.as_ref().ok_or_else(|| Err::<CanSocket, String>("Socket not initialized".to_string())).unwrap().write_frame(&frame).map_err(|err| err.to_string())?;
     Ok(())
 }
